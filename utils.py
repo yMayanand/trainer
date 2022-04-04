5a12b3f89e485379b508a98c04bab35e9588e527
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-04-04 14:46:25 +0530

    changes in utils

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
-def get_basic_writer(log_dir='runs/exp', comment=''):
-    writer = SummaryWriter(log_dir=log_dir, comment=comment)
-    layout = { "Trainig Metrics": {
+# for reference
+layout = { "Training_Metrics": {
         "loss": ["Multiline", ["loss/train", "loss/val"]],
         "accuracy": ["Multiline", ["accuracy/train", "accuracy/val"]],
         },
     }
+
+def create_layout(metrics):
+    temp = {}
+    for metric in metrics:
+        temp.update({metric: ["Multiline", [f"{metric}/train", f"{metric}/val"]]})
+    layout = {'training_metrics': temp}
+    return 
+
+def get_basic_writer(log_dir='runs/exp', comment='', layout):
+    writer = SummaryWriter(log_dir=log_dir, comment=comment)
     writer.add_custom_scalars(layout)
     return writer
